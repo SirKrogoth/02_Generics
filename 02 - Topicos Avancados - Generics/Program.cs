afe361f35788141b9fec5110b784c47beb8602a0
joao.fagundes <joao.fagundes@DSKDEV08>
2018-07-26 13:18:17 -0300

    Adicionar arquivos de projeto.

---
+++ 02 - Topicos Avancados - Generics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02___Topicos_Avancados___Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
